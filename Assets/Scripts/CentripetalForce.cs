a42b68f468bcd58c9bfca699138317629baf91de
Mohammed Bajaman <mbajaman@my.bcit.ca>
2022-04-03 23:47:14 -0700

    Added centripetal force and forward velocity

--- Assets/Scripts/CentripetalForce.cs
+++ Assets/Scripts/CentripetalForce.cs
@@ -2,17 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CentripetalForce : MonoBehaviour
-{
+public class CentripetalForce : MonoBehaviour {
+
+    private GameObject circleCenter;
+    private float angle;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        circleCenter = GameObject.FindGameObjectWithTag("ccenter");
+        angle = 60.0f;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate() { 
+        CalculateCentripetalForce();
     }
+
+    void CalculateCentripetalForce() {
+    if (circleCenter) {
+            Debug.Log("FOUND CENTER OF CIRCLE");
+
+            // Find the (r) distance between Object A and Object B:
+            //float distance = Vector3.Distance(transform.position, ObjectB.transform.position);
+
+            // Find (r^2) distance to the power of two; use Mathf.Pow: 
+            //float distanceSquared = Mathf.Pow(distance, 2);
+
+            // Find (Fg) magnitude of the gravity force; 
+            //float gravityMagnitude = (gravCoeeficient * ObjectA.GetComponent<PhysicsEngine>().mass * ObjectB.GetComponent<PhysicsEngine>().mass) / distanceSquared;
+            //Debug.Log("Gravity Magnitude: " + gravityMagnitude);
+            // Normalizing the gravity; Just uncomment the line below:
+            Vector3 offset = transform.position - circleCenter.transform.position;
+
+            float velocityMagnitude = GetComponent<PhysicsEngine>().velocityVector.magnitude;
+            //Vector3 velocityNormalized = GetComponent<PhysicsEngine>().velocityVector.normalized;
+
+            //Vector3 velocitySquared = Mathf.Pow(velocityMagnitude, 2) * velocityNormalized;
+
+            Vector3 centripetalForce = GetComponent<PhysicsEngine>().mass * Mathf.Pow(velocityMagnitude, 2) * offset.normalized / 30;
+            //Debug.Log(centripetalForce);
+            // Add the force to the list of the forces on physicsEngine for object A; 
+            // Note that you need to take care of the negative sign (downward acceleration) manually:
+
+            // Rotate the cube by converting the angles into a quaternion.
+            float tiltAroundZX = Input.GetAxis("Horizontal") * angle + -60;
+            Debug.Log(tiltAroundZX);
+            //angle += 4 * 0.1f;
+            Quaternion target = Quaternion.Euler(0, tiltAroundZX, 0);
+            transform.rotation = Quaternion.EulerRotation(0, tiltAroundZX, 0);
+            //transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 5.0f);
+            GetComponent<PhysicsEngine>().AddForce(-centripetalForce);
+        }
+}
 }
