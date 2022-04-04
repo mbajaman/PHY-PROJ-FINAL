1bba6f04c9b18f1b4f88f1b984c57e36a688349a
Gloria Ngo <gloria.nhuto.ngo@gmail.com>
2022-04-03 21:28:36 -0700

    added circuits + car physics + physics materials

---
+++ Assets/enableWheelPhysicMaterial.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enableWheelPhysicMaterial : MonoBehaviour {
+    private WheelCollider wheel;
+    void Start() {
+        wheel = GetComponent<WheelCollider>();
+    }
+    // static friction of the ground material.
+    void FixedUpdate() {
+        WheelHit hit;
+        if (wheel.GetGroundHit(out hit)) {
+            WheelFrictionCurve fFriction = wheel.forwardFriction;
+            fFriction.stiffness = hit.collider.material.staticFriction;
+            wheel.forwardFriction = fFriction;
+            WheelFrictionCurve sFriction = wheel.sidewaysFriction;
+            sFriction.stiffness = hit.collider.material.staticFriction;
+            wheel.sidewaysFriction = sFriction;
+        }
+    }
+}
